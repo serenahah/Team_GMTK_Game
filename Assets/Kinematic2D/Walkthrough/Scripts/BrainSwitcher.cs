62accbdb96f5381566adf325df2c4612fad1d983
Stephan-e <astephanerasmus@gmail.com>
2020-07-10 20:47:42 +0800

    Game template

---
+++ Assets/Kinematic2D/Walkthrough/Scripts/BrainSwitcher.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Lightbug.Kinematic2D.Implementation;
+
+public class BrainSwitcher : MonoBehaviour
+{
+    public CharacterHybridBrain brain = null;
+  
+    void Update()
+    {
+        if( Input.GetKeyDown(KeyCode.B) )
+            brain.SetBrainType( !brain.IsAI() );
+    }
+}
---
+++ Assets/tileshoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tileshoot : MonoBehaviour {
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+    private Vector2 screenBounds;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(speed, 0);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    
+    
+}
