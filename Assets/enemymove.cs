4cf96364fb2efe994f688b735fcac2b39a3b3f0e
anchitsh <31262184+anchitsh@users.noreply.github.com>
2020-07-12 16:07:19 +0530

    hello, is anyone reading this.

---
+++ Assets/enemymove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemymove : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float moveby;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void move()
+    {
+        rb.velocity = new Vector2(moveby, rb.velocity.y);
+    }
+}
